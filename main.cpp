453c714ed6bc3e605bf62161302bbf9f375b68e4
Boondock19 <josematiasgonzalez20@gmail.com>
2023-10-07 20:47:52 -0400

    Agregados mensajes de consola y arreglada logica del transvaso

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ class WaterBottle {
 
             if (firstBottleCurrentValue + secondBottleCurrentValue < secondBottleCapacity) {
                 secondBottle.setCurrentValue(firstBottleCurrentValue + secondBottleCurrentValue);
-                firstBottle.setCurrentValue(firstBottleCapacity - secondBottle.getCurrentValue());
+                firstBottle.setCurrentValue(firstBottleCurrentValue - secondBottle.getCurrentValue());
             } else {
                 secondBottle.setCurrentValue(secondBottleCapacity);
                 firstBottle.setCurrentValue(firstBottleCapacity - (secondBottleCapacity - secondBottleCurrentValue ));
@@ -69,6 +69,7 @@ class WaterBottle {
 int main()
 {
     cout << "Test de las propiedades de esta clase de WaterBottle" << endl;
+    cout << "Inicializacion de las botellas de agua:" << endl;
     WaterBottle bottle1(5);
     WaterBottle bottle2(3);
 
@@ -81,20 +82,28 @@ int main()
     cout << "Valor actual de la botella 1: " << bottle1.getCurrentValue() << endl;
     cout << "Valor actual de la botella 2: " << bottle2.getCurrentValue() << endl;
 
+    cout << "Transvasar de la botella 1 a la botella 2:  Primera vez" << endl;
     fillOtherBottle(bottle1, bottle2);
 
     cout << "Valor actual de la botella 1: " << bottle1.getCurrentValue() << endl;
     cout << "Valor actual de la botella 2: " << bottle2.getCurrentValue() << endl;
 
     bottle2.empty();
+    cout << "Vaciamos la botella 2: Primera vez"  << endl;
     cout << "Valor actual de la botella 1: " << bottle1.getCurrentValue() << endl;
     cout << "Valor actual de la botella 2: " << bottle2.getCurrentValue() << endl;
 
+    cout << "Transvasar de la botella 1 a la botella 2: Segunda vez" << endl;
     fillOtherBottle(bottle1, bottle2);
+    cout << "Valor actual de la botella 1: " << bottle1.getCurrentValue() << endl;
+    cout << "Valor actual de la botella 2: " << bottle2.getCurrentValue() << endl;
 
+    cout << "Llenamos la botella 1: Segunda vez" << endl;
     bottle1.fill();
     cout << "Valor actual de la botella 1: " << bottle1.getCurrentValue() << endl;
     cout << "Valor actual de la botella 2: " << bottle2.getCurrentValue() << endl;
+    
+    cout << "Transvasar de la botella 1 a la botella 2: Tercera vez" << endl;
     fillOtherBottle(bottle1, bottle2);
 
     cout << "Valor actual de la botella 1: " << bottle1.getCurrentValue() << endl;
